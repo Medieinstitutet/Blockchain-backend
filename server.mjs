27dbe333bad64a63b8e2d6d0e317ce1db0c049ab
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2024-07-05 10:01:07 +0200

    Add settings for pubnub server and crypto-lib

--- server.mjs
+++ server.mjs
@@ -9,9 +9,16 @@ import xss from 'xss-clean';
 import rateLimit from 'express-rate-limit';
 import hpp from 'hpp';
 import cors from 'cors';
+
+import Blockchain from './models/Blockchain.mjs';
+import TransactionPool from './models/TransactionPool.mjs';
+import Wallet from './models/Wallet.mjs';
+import blockRouter from './routes/blockRoutes.mjs';
+import blockchainRouter from './routes/blockchainRoutes.mjs';
+import transactionRouter from './routes/transactionRoutes.mjs';
 import authRouter from './routes/authRoutes.mjs';
-import coursesRouter from './routes/coursesRoutes.mjs';
 import usersRouter from './routes/userRoutes.mjs';
+import PubNubServer from './pubnubServer.mjs';
 
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -19,11 +26,28 @@ import { errorHandler } from './middleware/errorHandler.mjs';
 
 dotenv.config({ path: './config/config.env' });
 
+const credentials = {
+  publishKey: process.env.PUBLISH_KEY,
+  subscribeKey: process.env.SUBSCRIBE_KEY,
+  secretKey: process.env.SECRET_KEY,
+  userId: process.env.USER_ID,
+};
+
 connectDb();
 
 const fileName = fileURLToPath(import.meta.url);
 const dirname = path.dirname(fileName);
 global.__appdir = dirname;
+
+export const blockchain = new Blockchain();
+export const transactionPool = new TransactionPool();
+export const wallet = new Wallet();
+export const pubnubServer = new PubNubServer({
+  blockchain: blockchain,
+  transactionPool: transactionPool,
+  wallet: wallet,
+  credentials: credentials,
+});
 
 const app = express();
 
@@ -44,15 +68,50 @@ app.use(cors());
 app.use(hpp());
 
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/courses', coursesRouter);
 app.use('/api/v1/users', usersRouter);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5010;
+const DEFAULT_PORT = process.env.PORT;
+const ROOT_NODE = `http://localhost:${DEFAULT_PORT}`;
 
-const server = app.listen(PORT, () =>
-  console.log(`Server is running on port: ${PORT}`.yellow)
+let NODE_PORT;
+
+setTimeout(() => {
+  pubnubServer.broadcast();
+}, 1000);
+
+app.use('/api/v1/blockchain', blockchainRouter);
+app.use('/api/v1/block', blockRouter);
+app.use('/api/v1/wallet', transactionRouter);
+
+const synchronize = async () => {
+  let response = await fetch(`${ROOT_NODE}/api/v1/blockchain`);
+  if (response.ok) {
+    const result = await response.json();
+    blockchain.replaceChain(result.data);
+  }
+
+  response = await fetch(`${ROOT_NODE}/api/v1/wallet/transactions`);
+  if (response.ok) {
+    const result = await response.json();
+    transactionPool.replaceTransactionMap(result.data);
+  }
+};
+
+if (process.env.GENERATE_NODE_PORT === 'true') {
+  NODE_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 10);
+}
+
+const PORT = NODE_PORT || DEFAULT_PORT;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`.yellow);
+
+  if (PORT !== DEFAULT_PORT) {
+    synchronize();
+  }
+}
 );
 
 process.on('unhandledRejection', (err, promise) => {
