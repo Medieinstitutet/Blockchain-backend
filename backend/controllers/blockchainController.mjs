e27af3f07e14097e082610cd4ae85e7816af4267
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2024-07-06 11:20:26 +0200

    Fix: get blocks from mongoDB

--- backend/controllers/blockchainController.mjs
+++ backend/controllers/blockchainController.mjs
@@ -1,7 +1,10 @@
-import { blockchain } from '../server.mjs';
+// import { blockchain } from '../server.mjs';
+import BlockModel from '../models/BlockModel.mjs';
 
-export const listBlock = (req, res, next) => {
+export const listBlock = async (req, res, next) => {
+  const getBlocks = await BlockModel.find();
+
   res
     .status(200)
-    .json({ success: true, statusCode: 200, data: blockchain.chain });
+    .json({ success: true, statusCode: 200, data: getBlocks });
 };
