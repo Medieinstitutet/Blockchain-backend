13634e557eef20107ec58c892ea2df58cac853f0
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2024-07-09 17:30:00 +0200

    Move initBlockchain logic into initBlockchain.mjs

--- backend/controllers/blockController.mjs
+++ backend/controllers/blockController.mjs
@@ -1,21 +1,6 @@
 import { pubnubServer } from '../server.mjs';
 import { blockchain } from '../server.mjs';
-import BlockModel from '../models/BlockModel.mjs';
 import { asyncHandler } from '../middleware/asyncHandler.mjs';
-import { GENESIS_DATA } from '../config/settings.mjs';
-
-export const initializeBlockchain = async () => {
-  try {
-    const blockCount = await BlockModel.countDocuments();
-    
-    if (blockCount === 0) {
-      await BlockModel.create(GENESIS_DATA);
-      console.log('Genesis block added to the database');
-    }
-  } catch (error) {
-    console.error('Error initializing blockchain:', error);
-  }
-};
 
 export const mineBlock = asyncHandler(async (req, res, next) => {
   const block = blockchain.addBlock({ data: req.body });  
---
+++ backend/initBlockchain.mjs
@@ -0,0 +1,15 @@
+import { GENESIS_DATA } from "./config/settings.mjs";
+import BlockModel from "./models/BlockModel.mjs";
+
+export const initializeBlockchain = async () => {
+  try {
+    const blockCount = await BlockModel.countDocuments();
+    
+    if (blockCount === 0) {
+      await BlockModel.create(GENESIS_DATA);
+      console.log('Genesis block added to the database');
+    }
+  } catch (error) {
+    console.error('Error initializing blockchain:', error);
+  }
+};
